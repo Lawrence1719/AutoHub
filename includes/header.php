910d1587a20ff8d573b25a5fa0e25f29c7108ea4
LawrenceD. <lawrenceld0417@gmail.com>
2024-06-30 18:58:16 +0800

    modified:   Project Structure/Project Structure.txt 	modified:   css/styles.css 	new file:   images/CarAds.jpg 	new file:   images/CarAds1.jpg 	new file:   images/CarAds2.jpg 	modified:   includes/footer.php 	modified:   includes/header.php 	modified:   index.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,10 @@
+<?php
+session_start(); // Start the session to check login status
+
+// Check if the user is logged in
+$is_logged_in = isset($_SESSION['user_id']);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,17 +15,22 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <a class="navbar-brand" href="index.php">Auto Hub</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ml-auto">
                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="wallet.php">Wallet</a></li>
-                <li class="nav-item"><a class="nav-link" href="chat.php">Chat</a></li>
-                <li class="nav-item"><a class="nav-link" href="notifications.php">Notifications</a></li>
+                <li class="nav-item"><a class="nav-link" href="new_cars.php">New Cars</a></li>
+                <li class="nav-item"><a class="nav-link" href="used_cars.php">Used Cars</a></li>
+                <li class="nav-item"><a class="nav-link" href="trade_cars.php">Trade Cars</a></li>
+                <?php if ($is_logged_in): ?>
+                    <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
+                <?php else: ?>
+                    <li class="nav-item"><a class="nav-link" href="account.php">Account</a></li>
+                <?php endif; ?>
             </ul>
         </div>
     </nav>
